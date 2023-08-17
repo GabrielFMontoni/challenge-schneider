f5f17e9cb2839ca15c84a8229979217bde2a9964
GabrielFMontoni <gabrielf.montoni@gmail.com>
2023-08-17 18:46:47 -0300

    Melhorias no site

--- js/index.js
+++ js/index.js
@@ -1,8 +1,28 @@
 const bntPopUp = document.querySelector("#btn_popup");
-const modal = document.querySelector("dialog");
+const bntPitch = document.querySelector("#btn_popup2");
+const btnPerfil = document.querySelector('#mudarPerfil')
+const modal = document.querySelector("#popup-jogos");
+const modalPitch = document.querySelector('#pitch')
+const modalPerfil = document.querySelector('#perfil')
 const modalClose = document.querySelector("#modal_close");
-
+const modalClose2 = document.querySelector("#modal_close2");
+const modalClose3 = document.querySelector("#modal_close3");
 
 modalClose.addEventListener('click', ()=> modal.close());
 bntPopUp.addEventListener('click', () => modal.showModal());
 
+modalClose2.addEventListener('click', ()=> modalPitch.close());
+bntPitch.addEventListener('click', () => modalPitch.showModal());
+
+btnPerfil.addEventListener('click', () => modalPerfil.showModal());
+modalClose3.addEventListener('click', () => modalPerfil.close());
+
+
+
+
+const imagensPerfil = document.querySelectorAll('.img__perfil')
+
+imagensPerfil.forEach(img => img.addEventListener('click', function trocaIcone(){
+    mudarPerfil.src = img.src
+    modalPerfil.close()
+}))
